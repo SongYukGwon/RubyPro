860184ba94edf70b95838b5e5e5ebf75b6ff6ca5
SongYukGwon <thddbr1254@naver.com>
2022-05-18 03:08:15 +0900

    캐릭터 컨트롤러와 키보드 입력

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,14 +7,19 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*QualitySettings.vSyncCount = 0;
+        //초당 10프레임
+        Application.targetFrameRate = 10;*/
     }
 
-    // Update is called once per frame
+    // 프레임당 실행되는 함수이므로 프레임이 적어지면 실행빈도도 작아짐.
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
