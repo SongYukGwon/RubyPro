13f47c1ce637417d36e4cfd801de2877ace56bbf
SongYukGwon <thddbr1254@naver.com>
2022-05-20 00:27:19 +0900

    캐릭터이동제한

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+
     // Start is called before the first frame update
     void Start()
     {
-        /*QualitySettings.vSyncCount = 0;
-        //초당 10프레임
-        Application.targetFrameRate = 10;*/
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
-    // 프레임당 실행되는 함수이므로 프레임이 적어지면 실행빈도도 작아짐.
+    // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
+
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+
+        rigidbody2d.MovePosition(position);
     }
 }
